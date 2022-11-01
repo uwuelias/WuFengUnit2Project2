1d78d81998117bf50d73ffb99bd56874f1ef26a2
BT_3W2_21 <eliasw10@nycstudents.net>
2022-11-01 11:37:51 -0400

    line 16 and below needs modification

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,17 +1,17 @@
-/*
+
 import java.util.Scanner;
 public class LinearEquationRunner {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome!");
         System.out.print("Enter coordinate 1: ");
-        Integer.parseInt(scan.nextLine());
-        double x1 = scan.nextDouble();
-        double y1 = scan.nextDouble();
+        String coord1 = scan.nextLine();
+        int x1 = Integer.parseInt(coord1.substring(1, coord1.indexOf(",")));
+        int y1 = Integer.parseInt(coord1.substring(coord1.indexOf(",") + 2, coord1.indexOf(")")));
         System.out.print("\nEnter coordinate 2: ");
-        Integer.parseInt(scan.nextLine());
-        double x2 = scan.nextDouble();
-        double y2 = scan.nextDouble();
+        String coord2 = scan.nextLine();
+        int x2 = Integer.parseInt(coord2.substring(1, coord2.indexOf(",")));
+        int y2 = Integer.parseInt(coord2.substring(coord1.indexOf(",") + 2, coord2.indexOf(")")));
         LinearEquation coordinates = new LinearEquation(x1, y1, x2, y2);
         System.out.println();
         System.out.println("The two points are: " + "(" + x1 + ", " + y1 + ") and " + "(" + x2 + ", " + y2 + ")");
@@ -24,6 +24,4 @@ public class LinearEquationRunner {
         System.out.print("\n\nThe point on the line is " + coordinates.coordinateForX(xvalue));
     }
 }
-
-     */
 
